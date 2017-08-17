8fbe1bfff85647285577021a904399e7d16e7ac7
Jack <chen86860@126.com>
2017-08-17 11:02:40 +0800

    fix send arr

--- config/conf.js
+++ config/conf.js
@@ -7,9 +7,9 @@ module.exports = {
     database: 'user'
   },
   // 服务器地址
-  host: 'http://dev.adbats.com/',
+  host: 'http://cps.adbats.com/',
   // 发单间隔
-  TIME_INTERVAL: 1000 * 60 * 15,
+  TIME_INTERVAL: 1000 * 10,
   // 发单起止时间
   TIME_START_TIME: 8,
   TIME_END_TIME: 22
--- routes/chat.js
+++ routes/chat.js
@@ -16,7 +16,6 @@ let sendRobot = {
 
 module.exports = function (io) {
   router.get("/history/:group/:page/:size", (req, res) => {
-    console.log(req.params)
     if (req.params.page && req.params.size) {
       $http.get(API.getPost + req.params.group, {
         page: req.params.page,
@@ -37,8 +36,8 @@ module.exports = function (io) {
       })
     }
   })
+
   router.get("/room", (req, res) => {
-    console.log(req.query)
     if (req.query.group_id) {
       if (!socketArr.includes(req.query.group_id)) createSocket(req.query.group_id)
       res.render("chat", {
@@ -53,16 +52,7 @@ module.exports = function (io) {
       })
     }
   })
-  // router.get("/:id", (req, res) => {
-  //   if (req.cookies.username) {
-  //     if (!socketArr.includes(req.params.id)) createSocket(req.params.id)
-  //     res.render("chat", {
-  //       roomId: req.params.id
-  //     });
-  //   } else {
-  //     res.redirect('/')
-  //   }
-  // });
+
   // 根据id创建聊天群
   let createSocket = (id) => {
     let send_flag = true;
@@ -77,7 +67,7 @@ module.exports = function (io) {
           client.uid = user.id;
           user.name = user.name
           if (socketObj[id] && socketObj[id].onlineUsers && !socketObj[id].onlineUsers.hasOwnProperty(client.uid)) {
-            console.log('reg', user)
+            console.log('[', formatTime(new Date().getTime()), ']', 'Reg', user)
             socketObj[id].onlineUsers[client.uid] = {
               name: user.name,
               src: user.avatar
@@ -85,7 +75,7 @@ module.exports = function (io) {
           }
           // client.broadcast.emit("welcome", user);
           // client.broadcast.emit('updateCount', ++socketObj[id].onlineCount)
-          console.log(user.name + " 加入聊天室")
+          console.log('[', formatTime(new Date().getTime()), ']', user.name, " 加入聊天室")
         });
         client.on("newMsg", data => {
           // data.name = socketObj[id].onlineUsers[data.id]["name"] || ''
@@ -104,9 +94,9 @@ module.exports = function (io) {
               content_type: data.type || 1,
               send_time: data.time
             }).then(() => {
-              console.log('OK')
+              console.log('[', formatTime(new Date().getTime()), ']', '保存聊天记录OK')
             }).catch(() => {
-              console.log('bad')
+              console.log('[', formatTime(new Date().getTime()), ']', '保存聊天记录BAD')
             })
           }
 
@@ -148,9 +138,9 @@ module.exports = function (io) {
                     content_type: 1,
                     send_time: data.time
                   }).then(() => {
-                    console.log('OK')
+                    console.log('[', formatTime(new Date().getTime()), ']', '保存聊天记录OK')
                   })
-                  console.log('OK')
+                  console.log('[', formatTime(new Date().getTime()), ']', '保存聊天记录OK')
                 }).catch((err) => {
                   console.log(err)
                 })
@@ -164,7 +154,7 @@ module.exports = function (io) {
                   content_type: 1,
                   send_time: data.time
                 }).then(() => {
-                  console.log('OK')
+                  console.log('[', formatTime(new Date().getTime()), ']', '保存聊天记录OK')
                 })
               }
 
@@ -192,35 +182,115 @@ module.exports = function (io) {
           }, 0)
         });
 
-        // 定时发单
-        if (send_flag && (CONFIG.TIME_START_TIME - 1) < new Date().getHours() && new Date().getHours() < CONFIG.TIME_END_TIME) {
-          send_flag = false
-          let send = setInterval(() => {
+        // // 定时发单
+        // if (send_flag && (CONFIG.TIME_START_TIME - 1) < new Date().getHours() && new Date().getHours() < CONFIG.TIME_END_TIME) {
+        //   send_flag = false
+        //   let send = setInterval(() => {
+        //     $http.get(API.postOrder + id).then((res) => {
+        //       res = JSON.parse(res)
+
+        //       if (res.code === 200) {
+        //         res.img = res.data.image_url
+        //         res.cms_url = res.data.cms_url
+        //         res.info = res.data.content.replace(/(http.*)/ig, '<a class="link" href="$1">$1</a>').replace(/\n/ig, '<br>')
+        //       } else {
+        //         clearInterval(send)
+        //         res.info = '发单结束'
+        //       }
+        //       res.src = "../img/me.jpg"
+        //       res.id = 10000
+        //       res.name = '定时发单机器人'
+        //       res.time = new Date()
+        //       client.broadcast.emit('serverMsg', res)
+        //       client.emit('serverMsg', res)
+        //       console.log('[', new Date(), ']', '定时发单成功')
+
+
+        //       //保存找单图片
+        //       if (res.img) {
+        //         $http.post(API.savePost + id, {
+        //           user_id: res.id,
+        //           nickname: res.name,
+        //           head_portrait: res.src || '',
+        //           content: res.img,
+        //           content_type: 2,
+        //           send_time: data.time
+        //         }).then(() => {
+        //           //保存正常发单记录
+        //           $http.post(API.savePost + id, {
+        //             user_id: res.id,
+        //             nickname: res.name,
+        //             head_portrait: res.src || '',
+        //             content: res.info + `<br ><a class='link' href="${res.cms_url}">查看更多</a>`,
+        //             content_type: 1,
+        //             send_time: data.time
+        //           }).then(() => {
+        //             console.log('[', formatTime(new Date().getTime()), ']', '保存聊天记录OK')
+        //           })
+        //           console.log('[', formatTime(new Date().getTime()), ']', '保存聊天记录OK')
+        //         }).catch((err) => {
+        //           console.log(err)
+        //         })
+        //       }
+
+        //     }).catch((err) => {
+        //       console.log('[', new Date(), ']', '定时发单失败,err', err)
+        //     })
+        //   }, CONFIG.TIME_INTERVAL)
+        // } else {
+        //   send_flag = true
+        // }
+
+        let send = setInterval(function () {
+          // 定时发单
+          if (send_flag && (CONFIG.TIME_START_TIME - 1) < new Date().getHours() && new Date().getHours() < CONFIG.TIME_END_TIME) {
             $http.get(API.postOrder + id).then((res) => {
               res = JSON.parse(res)
+              // 有队列生成则一直发送
               if (res.code === 200) {
                 res.img = res.data.image_url
                 res.cms_url = res.data.cms_url
                 res.info = res.data.content.replace(/(http.*)/ig, '<a class="link" href="$1">$1</a>').replace(/\n/ig, '<br>')
+                res.src = "../img/me.jpg"
+                res.id = 10000
+                res.name = '定时发单机器人'
+                res.time = new Date()
+                client.broadcast.emit('serverMsg', res)
+                client.emit('serverMsg', res)
+
+                console.log('[', formatTime(new Date().getTime()), ']', '定时发单成功')
+
+                // 保存找单图片
+                if (res.img) {
+                  $http.post(API.savePost + id, {
+                    user_id: res.id,
+                    nickname: res.name,
+                    head_portrait: res.src || '',
+                    content: res.img,
+                    content_type: 2,
+                    send_time: res.time
+                  }).then(() => {
+                    //保存正常发单记录
+                    $http.post(API.savePost + id, {
+                      user_id: res.id,
+                      nickname: res.name,
+                      head_portrait: res.src || '',
+                      content: res.info + `<br ><a class='link' href="${res.cms_url}">查看更多</a>`,
+                      content_type: 1,
+                      send_time: res.time
+                    })
+                  })
+                }
               } else {
-                clearInterval(send)
-                res.info = '发单结束'
-                // send_flag = true
+                console.log('[', formatTime(new Date().getTime()), ']', '发单队列为空')
               }
-              res.src = "../img/me.jpg"
-              res.id = new Date().getTime()
-              res.name = '定时发单机器人'
-              res.time = new Date()
-              client.broadcast.emit('serverMsg', res)
-              client.emit('serverMsg', res)
-              console.log('[', new Date(), ']', '定时发单成功')
-            }).catch((err) => {
-              console.log('[', new Date(), ']', '定时发单失败,err', err)
+            }).catch(({ err }) => {
+              console.log('[', formatTime(new Date().getTime()), ']', '定时发单失败')
             })
-          }, CONFIG.TIME_INTERVAL)
-        } else {
-          send_flag = true
-        }
+          } else {
+            console.log('[', formatTime(new Date().getTime()), ']', '不在发单时间内')
+          }
+        }, CONFIG.TIME_INTERVAL)
       });
   }
 
@@ -236,6 +306,12 @@ module.exports = function (io) {
   //     socketArr: socketArr
   //   })
   // })
+  let formatTime = (val) => {
+    if (isNaN(val)) return ''
+    var prefix = (s) => s < 10 ? '0' + s : s
+    var date = new Date(val)
+    return date.getFullYear() + '-' + prefix((date.getMonth() + 1)) + '-' + prefix(date.getDate()) + ' ' + prefix(date.getHours()) + ':' + prefix(date.getMinutes()) + ':' + prefix(date.getSeconds())
+  }
 
   return router;
 };
