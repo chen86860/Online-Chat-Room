1cbee2342c1777046070f3540455e6b5610e1277
Jack <chen86860@126.com>
2017-08-17 19:47:52 +0800

    move path

--- app.js
+++ app.js
@@ -34,9 +34,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static('public'))
 app.use('/', index);
 app.use('/chat', chat)
+app.use('/chat', express.static('public'))
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
